6f7c5f2809ba2741e8225f876e0d89f89bf34202
joehurrie <joharikisiangani1@gmail.com>
2025-09-21 01:22:26 +0300

    refined UI

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
       container:{
         backgroundColor: "#90caf9",
       },
-      largerText: {
+      largeText: {
         fontSize: 30,
         color: "#f44336",
         marginBottom: 5,
@@ -15,23 +15,22 @@ import { Text, View, StyleSheet } from "react-native";
         color: "#9c27b0",
         marginBottom: 10,
         fontWeight: "500",
-        textAlign: "left",
+        textAlign: "right",
       },
       smallText:{
         fontSize: 15,
         color: "#2196f3",
         fontWeight: "400",
-        textAlign: "left",
+        textAlign: "center",
       },
     });
-
 export default function Index() {
   return (
     <View
       style={styles.container}
     >
-      
-      <Text style = {styles.largerText}>TypeScript is great if you practice more</Text>
+      <Text sytle = {styles.smallText}> Entry Screen - Awesome </Text>
+      <Text style = {styles.largeText}>TypeScript is great if you practice more</Text>
       <Text style ={ styles.mediumText}>React Native Provides You A Single Codebase for Cross Plartforms</Text>
       <Text style ={ styles.smallText}>ALX is awesome!</Text>
 
