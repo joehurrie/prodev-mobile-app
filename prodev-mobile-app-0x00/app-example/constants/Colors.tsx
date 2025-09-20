fd6866a81b5c3223a571f627248328a808658411
joehurrie <joharikisiangani1@gmail.com>
2025-09-21 00:39:20 +0300

    removed app-exaple from git ignore file

---
+++ prodev-mobile-app-0x00/app-example/constants/Colors.tsx
@@ -0,0 +1,10 @@
+// constants/colors.tsx
+const Colors = {
+  primary: '#2B876E',
+  secondary: '#FFA800',
+  background: '#FFFFFF',
+  text: '#0D0D0D',
+  error: '#f44336',
+};
+
+export default Colors;
