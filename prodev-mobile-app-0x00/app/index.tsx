a3c92dd89d85ebca349f573ac30cf0c1e70104c9
joehurrie <joharikisiangani1@gmail.com>
2025-09-20 23:24:11 +0300

    folder restructure

--- prodev-mobile-app-0x00/app/index.tsx
+++ prodev-mobile-app-0x00/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Hi Joharie, Welcome To Mobile App Dev.</Text>
     </View>
   );
 }
